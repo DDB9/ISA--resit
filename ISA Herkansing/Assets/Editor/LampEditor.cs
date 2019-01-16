cc2ed4e58de2aa0b1df2362a8866abd65cc31d6b
Daan de Bruijn <ddebruijn9@gmail.com>
2019-01-16 14:51:40 +0100

    Started on the Light Tool
    Making a light tool to easily create lights in the scene.

---
+++ ISA Herkansing/Assets/Editor/LampEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LampEditor : EditorWindow
+{
+    bool spotLight = false;
+
+    [MenuItem("Window/Lamp Editor")]
+    static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(LampEditor));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Create a lamp", EditorStyles.boldLabel);
+
+        GUILayout.Space(10f);
+
+        GUILayout.Label("Lamp type:");
+        spotLight = EditorGUILayout.Toggle("Spot Light", spotLight);
+
+        GUILayout.Space(10f);
+
+        if (GUILayout.Button("Create Lamp"))
+        {
+            GameObject go = new GameObject("New Light");
+            Light lt = go.AddComponent<Light>();
+            if (spotLight) lt.type = LightType.Spot;
+
+            if (GameObject.Find("Lights") != null) lt.transform.parent = GameObject.Find("Lights").transform;
+        }
+    }
+}
