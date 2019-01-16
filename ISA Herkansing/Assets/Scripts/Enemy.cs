3f1beecc702bf7dc064c77f0324ca65f24c77a4b
Daan de Bruijn <ddebruijn9@gmail.com>
2019-01-16 14:02:18 +0100

    Made some nice lämps.
    Brother I require oats. Enemy's no longer attack with a firerate of 60 attacks per second.

--- ISA Herkansing/Assets/Scripts/Enemy.cs
+++ ISA Herkansing/Assets/Scripts/Enemy.cs
@@ -4,35 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	public static Enemy instance = null;
+    public static Enemy instance = null;
 
-	public GameObject player;
+    public GameObject player;
     public float enemySpeed = 10f;
-	public float meleeRange;
-	public float attackRate;
-	public float meleeTimer;
-	
+    public float meleeRange;
+    public float attackRate;
+    public float meleeTimer;
+
+    // Declaring attack types.
+    private bool meleeAttack = false;
+    private bool rangedAttack = false;
+
+    private bool attacked = false;
+
     [SerializeField]
-	private float distanceFromPlayer;
+    private float distanceFromPlayer;
 
-	public static bool playerInSight;
+    public static bool playerInSight;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		player = GameObject.FindWithTag("Player");
-		//meleeTimer = Time.time + attackRate;
-		playerInSight = false;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        player = GameObject.FindWithTag("Player");
+        //meleeTimer = Time.time + attackRate;
+        playerInSight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
         if (playerInSight == true) PlayerInRange();
-	}
+    }
 
-    void OnTriggerStay(Collider other) { if (other.tag == "Player") playerInSight = true; } 
+    void OnTriggerStay(Collider other) { if (other.tag == "Player") playerInSight = true; }
 
     void OnTriggerExit(Collider other) { if (other.tag == "Player") playerInSight = false; }
 
@@ -42,19 +48,33 @@ public class Enemy : MonoBehaviour {
         playerPos.y = transform.position.y;
         transform.LookAt(playerPos);
 
-        if (distanceFromPlayer >= meleeRange) transform.position += transform.forward * enemySpeed * Time.deltaTime; 
-        if (distanceFromPlayer <= meleeRange) DealDamage();
+        if (distanceFromPlayer >= meleeRange) transform.position += transform.forward * enemySpeed * Time.deltaTime;
+        if (distanceFromPlayer <= meleeRange)
+        {
+            meleeAttack = true;
+            if (!attacked)
+            {
+                StartCoroutine("DealDamageOverTime");
+                attacked = true;
+            }
+        }
     }
 
-    public virtual void DealDamage()
+    IEnumerator DealDamageOverTime()
     {
-        if (Time.time > attackRate)
+        if (meleeAttack)
         {
+            yield return new WaitForSeconds(1.3f);
+
             Debug.Log("Attack!");
             playerController.playerLives -= 1;
             Debug.Log(playerController.playerLives.ToString());
             if (playerController.playerLives <= 0) Debug.Log("YOU DIED");
+
+            meleeAttack = false;
+            attacked = false;
         }
+
     }
 }
                                         
